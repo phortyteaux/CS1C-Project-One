98ce460d88b105e0cfdf960027efa0df967609ca
Johnny Wannamaker <78064684+jwannamaker@users.noreply.github.com>
2021-02-17 13:39:11 -0800

    Start Doxygen style documentation

--- Story3.cpp
+++ Story3.cpp
@@ -1,10 +1,20 @@
-// Story3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/**
+ * @file Story3.cpp
+ * @author Max Kwatcher and Johnny Wannamaker
+ * @brief This file contains the 'main' function. Program execution begins and ends there.
+ * @version 0.1
+ * @date 2021-02-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include "teamInfo.h"
 
-// function call operator overloads
-// will move these to a file for global definitions later
+/**
+ * @brief A function call operator overload. Will be moved into a global definition
+ * at a later date.
+ */
 struct sortByTeam
 {
     inline bool operator() (const teamInfo& obj1, const teamInfo& obj2)
@@ -12,6 +22,11 @@ struct sortByTeam
         return (obj1.getTeamName() < obj2.getTeamName());
     }
 };
+
+/**
+ * @brief A function call operator overload. Will be moved into a global definition
+ * at a later date.
+ */
 struct sortByStadium
 {
     inline bool operator() (const teamInfo& obj1, const teamInfo& obj2)
@@ -22,13 +37,21 @@ struct sortByStadium
 
 int main()
 {
+    /**
+     * @brief A file stream object initialized.
+     * 
+     */
     fstream file;
     teamInfo newTeam1, newTeam2, newTeam3;
     string temp = "";
 
     file.open("nfl.csv");
 
-    getline(file, temp); //removes line containing column titles
+    /**
+     * @brief Construct a new getline object that removes the column names from the input file.
+     * 
+     */
+    getline(file, temp); 
     
     newTeam1.read(file);
     newTeam2.read(file);
@@ -58,14 +81,3 @@ int main()
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
